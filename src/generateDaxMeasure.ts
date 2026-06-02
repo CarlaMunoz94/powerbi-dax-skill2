470ce77b2ed1bbdbf6806873844d916856ed7c68
CarlaMunoz94 <CARLA.MUNOZ@PEPSICO.COM>
2026-06-02 17:08:36 -0600

    Implement generateDaxMeasure function
    Add generateDaxMeasure function to create DAX measures.

--- src/generateDaxMeasure.ts
+++ src/generateDaxMeasure.ts
@@ -1,1 +1,14 @@
 
+import { buildPrompt } from "./buildPrompt";
+
+export async function generateDaxMeasure(userInput: string) {
+  const fakeModel = `
+Tables:
+Sales(Amount, Date)
+Date(Date)
+`;
+
+  const prompt = buildPrompt(fakeModel, userInput);
+
+  console.log("PROMPT:\n", prompt);
+}
