2a59c743b5563f44326b76bd20f885839b506fcf
CarlaMunoz94 <CARLA.MUNOZ@PEPSICO.COM>
2026-06-02 17:08:06 -0600

    Add buildPrompt function to read and format template

--- src/buildPrompt.ts
+++ src/buildPrompt.ts
@@ -1,1 +1,9 @@
+import fs from "fs";
 
+export function buildPrompt(modelMetadata: string, userInput: string) {
+  const template = fs.readFileSync("prompts/dax-measure-generator.md", "utf-8");
+
+  return template
+    .replace("{{MODEL_METADATA}}", modelMetadata)
+    .replace("{{USER_INPUT}}", userInput);
+}
